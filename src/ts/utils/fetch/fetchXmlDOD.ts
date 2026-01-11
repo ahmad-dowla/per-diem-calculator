742c90de37c64c17e88f19f7bb7f805cdca72829
ahmad-dowla <ahmad.dowla@gmail.com>
2026-01-11 15:53:24 -0600

    Gracefully handle transition period between years where DoD may put out a temporary locked ZIP file for first few weeks of January. That locked file will trigger an error in JZIP so switch to using prior year rates.

--- src/ts/utils/fetch/fetchXmlDOD.ts
+++ src/ts/utils/fetch/fetchXmlDOD.ts
@@ -4,7 +4,7 @@ import type { YYYY } from '../../types/dates';
 // Utils
 import JSZip from 'jszip';
 import { memoize } from './memoize';
-import { getYY } from '../dates';
+import { getYY, isYYYY } from '../dates';
 
 const PROXY_URL = import.meta.env.VITE_PROXY_URL;
 const PROXY_KEY = import.meta.env.VITE_PROXY_KEY;
@@ -25,7 +25,25 @@ const fetchXmlDOD = async (year: YYYY) => {
         throw new Error(`Failed to write file from ${url} to arrayBuffer`);
 
     const zip = new JSZip();
-    await zip.loadAsync(resFile);
+
+    try {
+        await zip.loadAsync(resFile);
+    } catch (e) {
+        // Gracefully handle transition period between years where DoD may put out a temporary locked ZIP file for first few weeks of January. That locked file will trigger an error in JZIP so we'll switch to using prior year rates.
+        const now = new Date();
+        const currentYear = now.getFullYear();
+        const currentMonth = now.getMonth() + 1;
+
+        if (+year === currentYear && currentMonth === 1) {
+            const fixedYear = (currentYear - 1).toString();
+            console.warn(
+                `${currentYear} data not yet public in finalized form. Falling back to ${fixedYear} data.`,
+            );
+            if (isYYYY(fixedYear)) return fetchXmlDOD(fixedYear);
+        }
+        // If it's not a current-year transition issue, throw the original error
+        throw e;
+    }
     const filename = `ocallhist-${getYY(year, 'YYYY')}.xml`;
     const data = await zip.file(filename)?.async('string');
     if (!data)
